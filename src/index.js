7be907e21479b44f65131034e87a5710e066634f
Tacal, Tudor D. (LNG-LON) <TACALTD@legal.regn.net>
2017-11-26 13:58:43 +0000

    Dispatch action to update state, install react-redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/App';
+import { createStore } from 'redux';
+import reducer from "./reducers";
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+ReactDOM.render(<App  store={store}/>, document.getElementById('root'));
 registerServiceWorker();
